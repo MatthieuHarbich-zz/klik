9cfae3249b878fb9759aadb019d419c554ea4f24
Matthieu Harbich <matthieu.harbich@gmail.com>
2015-03-02 22:50:25 +0100

    Preparing my repo

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('klikApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
